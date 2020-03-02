048be0441c5875f49f0b7a191aebd1b5471ebd4c
Tomas Nekvinda <tom@neqindi.cz>
2020-03-02 21:10:09 +0100

    Romanization of Japanese.

---
+++ dataset_prepare/mecab_convertor.py
@@ -0,0 +1,12 @@
+#encoding=utf-8
+
+import fileinput
+import romkan
+import MeCab
+
+wakati = MeCab.Tagger("-Owakati")
+yomi = MeCab.Tagger("-Oyomi")
+
+with open('mecab_output.txt', 'w') as f: 
+    for s in fileinput.input():
+        print(romkan.to_roma(yomi.parse(wakati.parse(s))), file=f)
